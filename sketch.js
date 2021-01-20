5cff67a650c0595526114f42861481f0b3d86e1f
DivayanshGaur2006 <76834576+DivayanshGaur2006@users.noreply.github.com>
2021-01-20 21:33:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+function preload() {
+    //load the images here
+
+    
+
+    
+
+ 
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+    Tom = createSprite(200,200,250,50);
+    
+    jerry = createSprite(600,200,250,100);
+
+}
+
+function draw() {
+
+    background(255);
+    
+    
+    
+      drawSprites();
+      
+}
+
+
